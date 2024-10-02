8725970d476a39ab33b1d8365ed92946daa03c40
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2024-10-02 09:04:48 +0700

    Initial commit

---
+++ src/main/java/vn/edu/likelion/front_ice/FrontIceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.likelion.front_ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontIceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontIceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/likelion/front_ice/FrontIceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.likelion.front_ice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontIceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
