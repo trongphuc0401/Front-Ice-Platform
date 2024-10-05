148abde041e33a29e0037e5b9080f076648bc4a2
NghiaPham <nghia01694049627@gmail.com>
2024-10-05 17:40:18 +0700

    feat: register, sendOTP, verifyOTP done

--- src/main/java/vn/edu/likelion/front_ice/common/constants/ApiEndpoints.java
+++ src/main/java/vn/edu/likelion/front_ice/common/constants/ApiEndpoints.java
@@ -20,6 +20,7 @@ public interface ApiEndpoints {
     String SIGNUP_VERIFY = "/signup-verify";  // POST
     String FORGOT_PASSWORD = "/forgot-password";  // POST
     String REFRESH_TOKEN = "/refresh-token";  // POST
+    String SEND_OTP = "/send-otp";  // POST
 
 
     String ID  = "{id}";
