fa6ba2defeab2d8979bb3e0c7c0ae813a2ba34d9
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2024-11-03 14:28:13 +0700

    release: 03-11-2024

--- src/main/java/vn/edu/likelion/front_ice/configuration/FirebaseConfig.java
+++ src/main/java/vn/edu/likelion/front_ice/configuration/FirebaseConfig.java
@@ -6,8 +6,12 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * FirebaseConfig -
@@ -20,7 +24,14 @@ import java.io.IOException;
 public class FirebaseConfig {
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("firebase.json");
+
+        String firebaseConfig = System.getenv("FIREBASE_CREDENTIALS");
+        if (firebaseConfig == null) {
+            throw new IllegalStateException("Environment variable FIREBASE_CREDENTIALS not set");
+        }
+
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("front-15551.appspot.com") // Thay YOUR_PROJECT_ID bằng ID của Firebase Project.
@@ -29,4 +40,5 @@ public class FirebaseConfig {
             FirebaseApp.initializeApp(options);
         }
     }
+
 }
